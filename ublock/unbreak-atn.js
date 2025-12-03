0400ccaa40719bb5e8c579665164a3f04c16bbf1
celenity <celenity@celenity.dev>
2025-12-02 22:10:35 -0500

    WIP
    Signed-off-by: celenity <celenity@celenity.dev>

--- ublock/unbreak-atn.js
+++ ublock/unbreak-atn.js
@@ -1,32 +1,22 @@
 unbreak-atn.js text/javascript
 (function() {
-    const targetPrefix = 'https://addons.thunderbird.net/user-media/addons/_attachments/';
-    
-    const originalFetch = window.fetch;
-    window.fetch = function(input, init) {
-        if (typeof input === 'string' && input.startsWith(targetPrefix)) {
-            // Clone the init object to avoid modifying the original
-            const modifiedInit = init ? {...init} : {};
-            
-            // Intercept and modify headers
-            if (!modifiedInit.headers) {
-                modifiedInit.headers = new Headers();
+    const originalXHR = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function(method, url) {
+        this.addEventListener('readystatechange', function() {
+            if (this.readyState === this.HEADERS_RECEIVED) {
+                if (url.includes('/user-media/addons/_attachments/')) {
+                    const headerNames = this.getAllResponseHeaders().split('\n');
+                    headerNames.forEach(header => {
+                        const name = header.split(':')[0].trim();
+                        if (name.toLowerCase() === 'content-disposition') {
+                            try {
+                                this.setRequestHeader(name, '');
+                            } catch(e) {}
+                        }
+                    });
+                }
             }
-            
-            // Remove content-disposition header if it exists
-            if (modifiedInit.headers instanceof Headers) {
-                modifiedInit.headers.delete('content-disposition');
-            } else if (Array.isArray(modifiedInit.headers)) {
-                modifiedInit.headers = modifiedInit.headers.filter(
-                    header => header[0].toLowerCase() !== 'content-disposition'
-                );
-            } else if (typeof modifiedInit.headers === 'object') {
-                delete modifiedInit.headers['content-disposition'];
-            }
-            
-            return originalFetch(input, modifiedInit);
-        }
-        
-        return originalFetch(input, init);
+        });
+        return originalXHR.apply(this, arguments);
     };
 })();
