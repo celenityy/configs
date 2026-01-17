c3484bd221e13f2946d17257e52cca804b99cd0e
celenity <celenity@celenity.dev>
2026-01-17 00:32:58 -0500

    fix: env tweaks
    Signed-off-by: celenity <celenity@celenity.dev>

--- linux/etc/profile.d/celenity-env.sh
+++ linux/etc/profile.d/celenity-env.sh
@@ -4,16 +4,16 @@
 export GST_VAAPI_ALL_DRIVERS=1;
 
 # Configure defaults
-export BROWSER="firefox -P default-release";
-export EDITOR="codium -w";
-export VISUAL="codium -w";
+export BROWSER='/opt/firefox/firefox -P default-release';
+export EDITOR='/usr/bin/nano';
+export VISUAL='/usr/bin/nano';
 
 # Configure Homebrew (if installed)
 export HOMEBREW_ASK=1;
 export HOMEBREW_AUTO_UPDATE_SECS=3600;
-export HOMEBREW_BROWSER="firefox -P default-release";
-export HOMEBREW_CASK_OPTS="--require-sha";
-export HOMEBREW_CURLRC="~/.curlrc";
+export HOMEBREW_BROWSER='/opt/firefox/firefox -P default-release';
+export HOMEBREW_CASK_OPTS='--require-sha';
+#export HOMEBREW_CURLRC="~/.curlrc";
 export HOMEBREW_DISPLAY_INSTALL_TIMES=1;
 export HOMEBREW_NO_ANALYTICS=1;
 export HOMEBREW_NO_ENV_HINTS=1;
@@ -71,130 +71,16 @@ export ELECTRON_OZONE_PLATFORM_HINT='auto';
 export WEBKIT_FORCE_SANDBOX=1;
 
 # For building IronFox...
-export SB_GAPI_KEY_FILE=/home/user/IronFox/sb-gapi.data;
+export SB_GAPI_KEY_FILE='/home/user/IronFox/sb-gapi.data';
 
 # Required for GPG
-export GPG_TTY=$(tty);
+# export GPG_TTY=$(tty);
 
 # Aliases
-alias envedit='/usr/bin/nano ~/.bashrc';
+alias envedit='/usr/bin/run0 /usr/bin/nano ~/.bashrc';
+# alias envedit='/usr/bin/nano ~/.bashrc';
 alias envup='source ~/.bashrc';
 alias git-up='/usr/bin/git commit -am 'Update' && git push';
 alias gits='/usr/bin/git commit --signoff';
-alias update='/usr/bin/sudo /usr/bin/dnf update --refresh && /usr/bin/sudo /usr/bin/bash /opt/celenity/Scripts/update_firefox.sh && /usr/bin/sudo /usr/bin/bash /opt/celenity/Scripts/update_thunderbird.sh && /usr/bin/flatpak --system update && /usr/bin/flatpak --user update && /usr/bin/sudo /usr/bin/fwupdmgr refresh --force && /usr/bin/sudo /usr/bin/fwupdmgr upgrade';
-
-# More aliases + helpers
-## Taken from Brace (AGPL-3.0-or-later): https://codeberg.org/divested/brace
-
-# Cleaning
-alias bleachLast='bleachbit --preset --clean';
-alias clearTracker='/usr/bin/tracker reset --hard';
-alias clearDriveFreeSpace='scrub --no-hwrand -p random -X cfs-$RANDOM';
-alias clearDriveDiscard='sudo fstrim -av';
-alias clearall='clearUserMisc && clearLogins && clearVarLog && clearVarLogMisc && clearJournal && clearMemory && clearDriveDiscard';
-
-clearUserMisc() {
-	#rm -fv ~/.*_history;
-	#rm -rfv ~/.esmtp_queue;
-	rm -fv ~/.lesshst;
-
-	rm -fv ~/.android/adbkey*;
-	rm -rfv ~/.cache/libvirt/qemu/log;
-	rm -rfv ~/.cache/thumbnails;
-	rm -fv ~/.clamtk/history/*.log;
-	rm -fv ~/.config/inkscape/extension-errors.log;
-	rm -rfv ~/.config/obs-studio/logs;
-	rm -rfv ~/.config/obs-studio/profiler_data;
-	rm -fv ~/.config/supertuxkart/stdout.log*;
-	#rm -fv ~/.config/wireshark/recent*;
-	#rm -rfv ~/.jack-server/logs;
-	rm -rfv ~/.local/share/gvfs-metadata;
-	rm -fv ~/.local/share/ibus-typing-booster/*debug.log;
-	rm -fv ~/.local/share/recently-used.xbel;
-	rm -fv ~/.minetest/debug.txt;
-
-	#rm -fv ~/.wget-hsts
-	#rm -fv ~/.gnutls/known_hosts;
-	#rm -fv ~/.ssh/known_hosts;
-}
-
-clearUsersMisc() {
-	#Contains list of all accessed/modified files on all connected drives (local or remote)
-	sudo wipe -irqc /home/*/.local/share/gvfs-metadata;
-}
-
-clearLogins() {
-	echo "" | sudo tee /var/log/btmp;
-	echo "" | sudo tee /var/log/wtmp;
-}
-
-clearVarLog() {
-	sudo find /var/log -type f -regex ".*\.gz$" -print -delete;
-	sudo find /var/log -type f -regex ".*\.log.[0-9]+$" -print -delete;
-	sudo find /var/log -type f -regex ".*\.log-[0-9]+$" -print -delete;
-}
-
-clearVarLogMisc() {
-	# libvirt
-	sudo rm -fv /var/log/libvirt/qemu/*.log*;
-	# misc
-	sudo rm -fv /var/log/grubby;
-	# package manangers
-	sudo rm -fv /var/log/dnf*.log;
-	sudo rm -fv /var/log/hawkey.log;
-	sudo rm -fv /var/log/yum*.log;
-	sudo rm -fv /var/log/pacman.log;
-	# scanners
-	sudo rm -fv /var/log/lynis*;
-	sudo rm -fv /var/log/rkhunter.log*;
-	sudo rm -fv /var/log/freshclam.log;
-	sudo rm -fv /var/log/clamav/*.log;
-	# security
-	sudo rm -fv /var/log/audit/audit.log;
-	sudo rm -fv /var/log/usbguard/usbguard-audit.log;
-	# systemd
-	sudo rm -fv /var/log/boot.log;
-}
-
-clearJournal() {
-	sudo journalctl --rotate;
-	sudo journalctl --vacuum-files=1 --vacuum-size=1K --vacuum-time=1s;
-	sudo journalctl --flush;
-}
-
-clearMemory() {
-	free -m;
-	sudo sync; #sudo isn't necessary for this, but ensures the next command is likely to run without delay
-	echo 3 | sudo tee /proc/sys/vm/drop_caches;
-	free -m;
-}
-
-optimizeImages() {
-        find "$1" -type f -name "*.jp*g" -print0 | xargs -0 -n1 -P 16 jpegoptim --strip-all;
-        find "$1" -type f -name "*.png" -print0 | xargs -0 -n1 -P 16 optipng -strip all;
-}
-
-loosePermsRecursive() {
-	echo "Recursively setting loose permissions in $PWD";
-	echo "You've 5 seconds to Ctrl+C";
-	sleep 5;
-	find . -type d -print0 | xargs -0 chmod -v 0755;
-	find . -type f -print0 | xargs -0 chmod -v 0644;
-}
-
-strictPermsRecursive() {
-	echo "Recursively setting restrictive permissions in $PWD";
-	echo "You've 5 seconds to Ctrl+C";
-	sleep 5;
-	find . -type d -print0 | xargs -0 chmod -v 0700;
-	find . -type f -print0 | xargs -0 chmod -v 0600;
-}
-
-
-# Malware scanning
-alias clamscanhere='clamscan --recursive --detect-pua=yes --infected --allmatch --cross-fs=no --follow-dir-symlinks=0 --follow-file-symlinks=0 .';
-alias rootkitscan='sudo rkhunter --update && sudo rkhunter -c --enable all --disable none --rwo';
-alias rootkitscanalt='sudo unhide reverse procall sys';
-
-# Misc.
-alias ssh-tor='torsocks --isolate ssh';
+alias update='/usr/bin/rpm-ostree upgrade && /usr/bin/run0 /usr/bin/bash /opt/celenity/Scripts/update_firefox.sh && /usr/bin/run0 /usr/bin/bash /opt/celenity/Scripts/update_thunderbird.sh && /usr/bin/flatpak --system update && /usr/bin/flatpak --user update && /usr/bin/fwupdmgr refresh --force && /usr/bin/fwupdmgr upgrade';
+#alias update='/usr/bin/sudo /usr/bin/dnf update --refresh && /usr/bin/sudo /usr/bin/bash /opt/celenity/Scripts/update_firefox.sh && /usr/bin/sudo /usr/bin/bash /opt/celenity/Scripts/update_thunderbird.sh && /usr/bin/flatpak --system update && /usr/bin/flatpak --user update && /usr/bin/fwupdmgr refresh --force && /usr/bin/fwupdmgr upgrade';
